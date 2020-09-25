c8447fc816608ebe1f2c854d0c3230f2b70d9844
Riaz <riazaham@gmail.com>
2020-09-25 15:54:44 +0800

    Date formats added

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,18 +1,35 @@
 package duke;
 
-public class DukeException extends Exception {
-    private String typeOfTask;
+import java.util.Arrays;
 
-    public DukeException(String typeOfTask) {
-        this.typeOfTask = typeOfTask;
+import static duke.Duke.taskList;
+
+public class DukeException extends Exception {
+    private String error;
+    private final String[] taskTypes = new String[] {"todo","deadline","event", "done", "delete", "find"};
+    private final String[] taskIndexOutOfBounds = new String[] {"done OOB", "delete OOB"};
+    private final String[] taskIndexNegative = new String[] {"done negative", "delete negative"};
+
+    public DukeException(String error) {
+        this.error = error;
     }
 
-    public String getTypeOfTask() {
-        return typeOfTask;
+    public String getError() {
+        return error;
     }
 
     @Override
     public String toString() {
-        return "OOPS!!! The description of a " + typeOfTask + " cannot be empty!";
+        if (Arrays.asList(taskTypes).contains(error)) {
+            return "OOPS!!! The description of a " + error + " cannot be empty!";
+        }
+        else if (Arrays.asList(taskIndexOutOfBounds).contains(error)) {
+            return "OOPS!!! No such task exist\n" +
+                    "There are only " + taskList.getList().size() + " tasks in your list";
+        }
+        else if (Arrays.asList(taskIndexNegative).contains(error)) {
+            return "OOPS!!! Please input a valid task number greater than 0";
+        }
+        return "OOPS!!! Unknown error occurred";
     }
 }
